36c965ac6a892ebe700507b6e4dcbf3c5364c5b4
BHjr132 <bhjr132@gmail.com>
2018-09-01 12:55:55 +1000

    Add files via upload

---
+++ figure.js
@@ -0,0 +1,142 @@
+var figure = {
+    "frames": [], 
+    "layout": {
+        "autosize": true, 
+        "title": "Dwarven District 8values Results", 
+        "yaxis": {
+            "title": "B"
+        }, 
+        "scene": {
+            "yaxis": {
+                "range": [
+                    0, 
+                    100
+                ], 
+                "type": "linear", 
+                "autorange": false
+            }, 
+            "aspectmode": "auto", 
+            "camera": {
+                "eye": {
+                    "y": 1.7606522133939568, 
+                    "x": 2.58827601271677, 
+                    "z": 1.3885988964255953
+                }, 
+                "up": {
+                    "y": 0, 
+                    "x": 0, 
+                    "z": 1
+                }, 
+                "center": {
+                    "y": 0, 
+                    "x": 0, 
+                    "z": 0
+                }
+            }, 
+            "zaxis": {
+                "range": [
+                    0, 
+                    100
+                ], 
+                "type": "linear", 
+                "autorange": false
+            }, 
+            "xaxis": {
+                "range": [
+                    0, 
+                    100
+                ], 
+                "type": "linear", 
+                "autorange": false
+            }, 
+            "aspectratio": {
+                "y": 1, 
+                "x": 1, 
+                "z": 1
+            }
+        }, 
+        "breakpoints": [], 
+        "shapes": [], 
+        "xaxis": {
+            "title": "A"
+        }, 
+        "hovermode": "closest", 
+        "showlegend": false, 
+        "legend": {
+            "traceorder": "normal", 
+            "orientation": "v"
+        }
+    }, 
+    "data": [
+        {
+            "autobinx": true, 
+            "name": "C", 
+            "zsrc": "BHjr132:0:793ebc", 
+            "ysrc": "BHjr132:0:56dcdb", 
+            "xsrc": "BHjr132:0:6519b0", 
+            "mode": "markers", 
+            "marker": {
+                "colorsrc": "BHjr132:0:b99c27", 
+                "autocolorscale": false, 
+                "cmin": 0, 
+                "colorscale": [
+                    [
+                        0, 
+                        "rgb(5, 10, 172)"
+                    ], 
+                    [
+                        0.35, 
+                        "rgb(106, 137, 247)"
+                    ], 
+                    [
+                        0.5, 
+                        "rgb(190, 190, 190)"
+                    ], 
+                    [
+                        0.6, 
+                        "rgb(220, 170, 132)"
+                    ], 
+                    [
+                        0.7, 
+                        "rgb(230, 145, 90)"
+                    ], 
+                    [
+                        1, 
+                        "rgb(178, 10, 28)"
+                    ]
+                ], 
+                "color": [
+                    "soc", 
+                    "72"
+                ], 
+                "reversescale": false, 
+                "colorbar": {
+                    "titleside": "top", 
+                    "title": "More Red: Communist<br>More Blue: Capitalist"
+                }, 
+                "cauto": false, 
+                "showscale": true, 
+                "cmax": 100
+            }, 
+            "textsrc": "BHjr132:0:d0ec32", 
+            "text": [
+                "name", 
+                "BHjr"
+            ], 
+            "y": [
+                "lib", 
+                "70"
+            ], 
+            "x": [
+                "globalism", 
+                "67"
+            ], 
+            "z": [
+                "prog", 
+                "82"
+            ], 
+            "type": "scatter3d", 
+            "autobiny": true
+        }
+    ]
+}
